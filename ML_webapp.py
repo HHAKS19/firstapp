de27ca107d7f94a4a0b55f9a6d2f08a46a54e833
HHAKS19 <65529499+HHAKS19@users.noreply.github.com>
2021-09-19 15:56:02 +0630

    Add files via upload

---
+++ ML_webapp.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+import numpy as np 
+
+from sklearn import datasets
+
+st.title('Streamlit Example')
+
+st.write("""
+# Explore different classifier and datasets
+Which one is the best?
+""")
+
+dataset_name = st.sidebar.selectbox(
+    'Select Dataset',
+    ('Iris', 'Breast Cancer', 'Wine')
+)
+
+st.write(f"## {dataset_name} Dataset")
+
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('KNN', 'SVM', 'Random Forest')
+)
+
+def get_dataset(dataset_name):
+    data = None
+    if dataset_name == 'Iris':
+        data = datasets.load_iris()
+    elif dataset_name == 'Wine':
+        data = datasets.load_wine()
+    else:
+        data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write('Shape of dataset:', X.shape)
+st.write('number of classes:', len(np.unique(y)))
